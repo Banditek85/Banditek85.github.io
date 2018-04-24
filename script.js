8b5cbfb6434d5b78d164452a14ed0bf0277defea
Mitja Sušec <mitjasusec@outlook.com>
2018-04-24 20:19:20 +0200

    update content

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 let navbar = document.querySelector(".nav");
+let grid = document.querySelector(".grid");
+let projects = document.querySelectorAll(".grid div");
+
 
 window.addEventListener("scroll", function(){
-    if (this.pageYOffset > 300) {
+    if (this.pageYOffset > 350) {
         navbar.classList.add("nav-transparent");
     }
 
     else (navbar.classList.remove("nav-transparent"));
 });
+
+grid.addEventListener("click", function(event) {
+    let clicked = event.target.parentNode;
+
+    console.log(clicked);
+    
+
+});
+
