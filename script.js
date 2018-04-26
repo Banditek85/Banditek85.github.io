9473b0f014561ce5154b1978a7534967985a0329
Mitja Sušec <mitjasusec@outlook.com>
2018-04-26 21:44:43 +0200

    add header animation

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
-let navbar = document.querySelector(".nav");
-let nav = document.querySelector(".nav ul");
-let burger = document.querySelector(".fa-bars");
+function addListeners() {
+    let burger = document.querySelector(".fa-bars");
+    let navbar = document.querySelector(".nav");
 
-window.addEventListener("scroll", function(){
-    if (this.pageYOffset > 350) {
-        navbar.classList.add("nav-transparent");
+    window.addEventListener("scroll", function () {
+        if (this.pageYOffset > 350) {
+            navbar.classList.add("nav-transparent");
+        } else(navbar.classList.remove("nav-transparent"));
+    });
+
+    burger.addEventListener("click", function () {
+        let nav = document.querySelector(".nav ul");
+        nav.classList.toggle("show");
+    });
+}
+
+function type() {
+    const headerText = document.querySelector(".header-text");
+    const h1 = headerText.children[0];
+    const h2 = headerText.children[1];
+    const h1Text = "Hello world!";
+    const h2Text = "I am a web developer";
+    let i = 0;
+    let j = 0;
+    const speed = 250;
+
+    function typeWriter() {
+        if (i < h1Text.length) {
+            h1.textContent += h1Text.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+        } else if (j < h2Text.length) {
+            h2.textContent += h2Text.charAt(j).toUpperCase();
+            j++;
+            setTimeout(typeWriter, speed);
+        }
     }
-    else (navbar.classList.remove("nav-transparent"));
-});
+    setTimeout(typeWriter, 700);
+}
 
-burger.addEventListener("click", function(){
-    nav.classList.toggle("show");
-});
+addListeners();
+type();
