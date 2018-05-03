ac8829a21fd416e03e6d31169f861dfe0b4bb6a3
Mitja Sušec <mitjasusec@outlook.com>
2018-05-03 20:54:38 +0200

    add loading spinner

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
-function addListeners() {
-    let burger = document.querySelector(".fa-bars");
-    let navbar = document.querySelector(".nav");
-
-/** Navbar black color listener */
-    window.addEventListener("scroll", function () {
-        if (this.pageYOffset > 150) {
-            navbar.classList.add("nav-transparent");
-        } else(navbar.classList.remove("nav-transparent"));
-    });
-
-/** Burger menu click listener */
-    burger.addEventListener("click", function () {
-        let nav = document.querySelector(".nav ul");
-        nav.classList.toggle("show");
-    });
-}
-
-function type() {
-    const headerText = document.querySelector(".header-text");
-    const text = headerText.children[1];
-    const h2Text = "I am a web developer";
-    let i = 0;
-    const speed = 150;
-
-    function typeWriter() {
-        if (i < h2Text.length) {
-            text.textContent += h2Text.charAt(i).toUpperCase();
-            i++;
-            setTimeout(typeWriter, speed);
-        }
+window.addEventListener("load", function(){
+    function addListeners() {
+        let burger = document.querySelector(".fa-bars");
+        let navbar = document.querySelector(".nav");
+    
+    /** Navbar black color listener */
+        window.addEventListener("scroll", function () {
+            if (this.pageYOffset > 150) {
+                navbar.classList.add("nav-transparent");
+            } else(navbar.classList.remove("nav-transparent"));
+        });
+    
+    /** Burger menu click listener */
+        burger.addEventListener("click", function () {
+            let nav = document.querySelector(".nav ul");
+            nav.classList.toggle("show");
+        });
     }
-    setTimeout(typeWriter, 700);
-}
+    
+    function type() {
+        const headerText = document.querySelector(".header-text");
+        const text = headerText.children[1];
+        const h2Text = "I am a web developer";
+        let i = 0;
+        const speed = 150;
+    
+        function typeWriter() {
+            if (i < h2Text.length) {
+                text.textContent += h2Text.charAt(i).toUpperCase();
+                i++;
+                setTimeout(typeWriter, speed);
+            }
+        }
+        setTimeout(typeWriter, 1200);
+    }
 
-addListeners();
-type();
+    let loadOverlay = document.getElementById("loading-overlay");
+    setTimeout(() => {
+        loadOverlay.style.display = "none";
+    }, 500);
+    addListeners();
+    type();
+});
